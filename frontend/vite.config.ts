4d2e69d451f8630207e51e33c6aa0b53b9c89778
gmezan <meza.gustavo@pucp.edu.pe>
2026-06-30 12:24:47 -0500

    ci: workflow for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: isGitHubPages ? '/app-hub-platform/' : '/',
 })
